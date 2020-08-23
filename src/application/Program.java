3e9b2442afcb72a3f347fbbe428c88c547bfee03
IsmarleyA7X <ismarley_2005@msn.com>
2020-08-23 13:32:23 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dpt = new Department(1, "Books");
+		Seller seller = new Seller(21, "Ismarley", "exemplo@gmail.com", new Date(), 3000.0, dpt);
 		
-		System.out.println(dpt);
+		System.out.println(seller);
 
 	}
 
